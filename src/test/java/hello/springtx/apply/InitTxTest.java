1302a333ab7253cb47fd91491ccebb9a824ddc26
Judalm Park (dami) <wnekfa1004@naver.com>
2023-05-04 20:28:30 +0900

    트랜잭션 AOP 주의 사항 - 초기화 시점
    초기화 코드가 먼저 호출되고, 그 다음에 트랜잭션 AOP가 적용된다

--- src/test/java/hello/springtx/apply/InitTxTest.java
+++ src/test/java/hello/springtx/apply/InitTxTest.java
@@ -4,9 +4,11 @@ package hello.springtx.apply;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
@@ -25,7 +27,7 @@ public class InitTxTest {
     }
 
     @TestConfiguration
-    static class InitTxTestConfig{
+    static class InitTxTestConfig {
 
         @Bean
         Hello hello() {
@@ -33,7 +35,7 @@ public class InitTxTest {
         }
     }
 
-    static class  Hello {
+    static class Hello {
 
         @PostConstruct
         @Transactional
@@ -43,8 +45,11 @@ public class InitTxTest {
 
         }
 
+        @EventListener(ApplicationReadyEvent.class)
+        @Transactional
         public void initV2() {
-
+            boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Hello init @ApplicationReadyEvent tx active={}", isActive);
         }
     }
 }
